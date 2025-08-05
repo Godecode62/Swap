bfbbdade4e2d989fc9a468966fd53aabbc73439f
Godecode62 <kikoyolo63@gmail.com>
2025-08-05 20:29:53 +0200

    feat : log registrations

--- Swap/settings.py
+++ Swap/settings.py
@@ -183,3 +183,23 @@ MEDIA_URL = f"https://{CLOUDFLARE_R2_BUCKET}.{CLOUDFLARE_R2_ENDPOINT}/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
