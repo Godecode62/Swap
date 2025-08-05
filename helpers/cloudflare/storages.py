8eb075d6ca9106723309ca313b207e46ba222a64
Godecode62 <kikoyolo63@gmail.com>
2025-08-05 21:04:34 +0200

    feat : helpers update

--- helpers/cloudflare/storages.py
+++ helpers/cloudflare/storages.py
@@ -1,4 +1,10 @@
-from storages.backends.s3 import S3Storage
+# from storages.backends.s3 import S3Storage
 
-class MediaFilesStorage(S3Storage):
+# class MediaFilesStorage(S3Storage):
+#     location = 'media'
+
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class MediaFilesStorage(S3Boto3Storage):
     location = 'media'
