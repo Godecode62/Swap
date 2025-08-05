cc57a0fae7dbb16facf7d82e09a768d9d35b3c4f
Godecode62 <kikoyolo63@gmail.com>
2025-08-05 20:02:45 +0200

    feat: whitenoise added

--- Swap/urls.py
+++ Swap/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
